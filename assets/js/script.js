48f2c96febce977eaa13ec496d88e3eae385587c
Nathan Tshimbalanga <nathantshimbalaga1@gmail.com>
2025-01-30 23:32:47 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-window.onload = ()=>{
-    const intro = document.getElementById('intro');
-    const acceuilSection = document.getElementById('home-section');
-    const words = document.querySelectorAll('.word');
-
-    setTimeout(()=>{
-        intro.classList.add('animation');
-        setTimeout(()=>{
-            intro.style.display = "none";
-            acceuilSection.classList.add('show');
-        },1000)
-    },6000)
-}
-
 
 let navMenu = document.getElementById("nav-menu");
 let navContent = document.querySelector(".nav-menu-content")
