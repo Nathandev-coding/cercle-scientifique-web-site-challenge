0891e38e6299fc488dfc03f4533c0b141ae0338d
Nathan Tshimbalanga <nathantshimbalaga1@gmail.com>
2025-02-01 19:56:31 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,31 @@ navToggle.addEventListener("click", ()=>{
     navMenu.classList.toggle("show-menu");
     navToggle.classList.toggle("anime-toggle");
 })
+/*=========== ANIMATION SCROLL ===========*/
+//function to check if the elements are in the view port
+function isInView(element){
+    const rect = element.getBoundingClientRect();
+    return rect.top >=0 && rect.bottom <= window.innerHeight;
+}
+//applied animation to elements if they are in the view port
+function animationScrol(){
+    const boxs = document.querySelectorAll(".box");
+    boxs.forEach(box => {
+        if(isInView(box)){
+            if(box.classList.contains("box")){
+                box.classList.add('anime');
+            }else if(box.classList.contains('from-top')){
+                box.style.transform = "translateY(0)";
+                box.classList.add("top")
+            }else if(box.classList.contains('zoom-in')){
+                box.style.transform = "translateY(0)";
+                box.classList.add("zoom")
+            } 
+        }
+    }); 
+}
+/*=== initial call to check if the elements are in view ===*/
+animationScrol();
+/*===== listen to the scroll event ====*/
+window.addEventListener("scroll", animationScrol);
+
